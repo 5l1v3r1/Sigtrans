f41213f94bdea36b1c494446d5c10b26de885b18
Natal Pereira Junior <juniortl2005@gmail.com>
2017-03-22 12:13:52 -0300

    Material Design

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 import MetisMenu from 'react-metismenu';
 import RouterLink from 'react-metismenu-router-link';
+import FontIcon from 'react-toolbox/lib/font_icon';
 
-const content=[
+/*import { AppBar, Checkbox, IconButton } from 'react-toolbox';
+import { Layout, NavDrawer, Panel, Sidebar } from 'react-toolbox';*/
+
+const menu=[
     {
         icon: 'dashboard',
-        label: 'Home',
+        label: 'Sigtrans',
         to: '/',
     },
     {
@@ -15,64 +19,76 @@ const content=[
             {
                 icon: 'bolt',
                 label: 'Abertas',
-                content: [
-                    {
-                        icon: 'bolt',
-                        label: 'Mais abertas',
-                        to: '/ocorrencias',
-                    },
-                    {
-                        icon: 'bolt',
-                        label: 'Mais abert1as',
-                        to: '/',
-                    },
-                    {
-                        icon: 'bolt',
-                        label: 'Mais a2bertas',
-                        to: '/',
-                    }
-                ],
-            },{
+                to:'/ocorrencias/abertas'
+            },         
+            {
                 icon: 'bolt',
-                label: 'fechadas',
-                content: [
-                    {
-                        icon: 'bolt',
-                        label: 'Mais abertas',
-                        to: '/',
-                    },
-                    {
-                        icon: 'bolt',
-                        label: 'Mais abert1as',
-                        to: '/',
-                    },
-                    {
-                        icon: 'bolt',
-                        label: 'Mais a2bertas',
-                        to: '/',
-                    }
-                ],
-            }
+                label: 'Fechadas',
+                to: '/ocorrencias/fechadas',
+            }       
         ]
     }
 ];
 
 class App extends Component {
+ /*       state = {
+        drawerActive: false,
+        drawerPinned: false,
+        sidebarPinned: false
+    };
+
+    toggleDrawerActive = () => {
+        this.setState({ drawerActive: !this.state.drawerActive });
+    };
+
+    toggleDrawerPinned = () => {
+        this.setState({ drawerPinned: !this.state.drawerPinned });
+    }
+
+    toggleSidebar = () => {
+        this.setState({ sidebarPinned: !this.state.sidebarPinned });
+    };*/
+
 	render(){
 		return(
 			<div>
 				{/*<ul className="navigation">*/}
 				<div className="navigation">
-					<MetisMenu content={content} LinkComponent={RouterLink} classNameContainer="pure" />
+					<MetisMenu content={menu} LinkComponent={RouterLink} />
 				</div>
 				{/*</ul>*/}
 
 				<input type="checkbox" id="nav-trigger" className="nav-trigger" />
-				<label htmlFor="nav-trigger"></label>
+				<label htmlFor="nav-trigger"><FontIcon className="md-24" value='menu'/></label>
 
 				<div className="site-wrap">
 					{this.props.children}
 				</div>
+{/*
+                <Layout>
+                    <NavDrawer active={this.state.drawerActive}
+                        pinned={this.state.drawerPinned} permanentAt='xxxl'
+                        onOverlayClick={ this.toggleDrawerActive }>
+                        <p>
+                            Navigation, account switcher, etc. go here.
+                        </p>
+                    </NavDrawer>
+                    <Panel>
+                        <AppBar leftIcon='menu' onLeftIconClick={ this.toggleDrawerActive } />
+                        <div style={{ flex: 1, overflowY: 'auto', padding: '1.8rem' }}>
+                            <h1>Main Content</h1>
+                            <p>Main content goes here.</p>
+                            <Checkbox label='Pin drawer' checked={this.state.drawerPinned} onChange={this.toggleDrawerPinned} />
+                            <Checkbox label='Show sidebar' checked={this.state.sidebarPinned} onChange={this.toggleSidebar} />
+                        </div>
+                    </Panel>
+                    <Sidebar pinned={ this.state.sidebarPinned } width={ 5 }>
+                        <div><IconButton icon='close' onClick={ this.toggleSidebar }/></div>
+                        <div style={{ flex: 1 }}>
+                            <p>Supplemental content goes here.</p>
+                        </div>
+                    </Sidebar>
+                </Layout>*/}
 			</div>
 			);
 	}
